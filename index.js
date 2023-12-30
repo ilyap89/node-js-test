75f8490b325d87b60ff3a734b5108ee7ccbae467
ilyap89 <ilyaap89@gmail.com>
2023-12-30 21:34:51 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,3 @@
-//var http = require('http');
-
-//create a server object:
-//http.createServer(function (req, res) {
- // res.write('Ilya NodeJS Lab'); //write a response to the client
-//  res.end(); //end the response
-//}).listen(3000); //the server object listens on port 80
-
 const http = require('http');
 const hostname = '172.31.26.45'; // Make sure to change this to your EC2 private IP
 const port = 3000;
