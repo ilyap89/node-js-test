4f7c1f3e2d681bb55f9b1f97dcf93367d86dfc7e
ilyap89 <ilyaap89@gmail.com>
2024-01-03 12:53:08 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
 const http = require('http');
+const fs = require('fs');
+const path = require('path');
 const hostname = '172.31.17.34'; // Make sure to change this to your EC2 private IP
 const port = 3000;
 const server = http.createServer((req, res) => {
- res.statusCode = 200;
- res.setHeader('Content-Type', 'text/plain');
- res.end('Hello World from Ilya Test Lab!');
+    if (req.url === '/' || req.url === '/index.html') {
+        const filePath = path.join(__dirname, 'index.html');
+        const readStream = fs.createReadStream(filePath);
+
+        // Set the content type to HTML
+        res.setHeader('Content-Type', 'text/html');
+
+        // Pipe the read stream to the response
+        readStream.pipe(res);
+    } else {
+        // Handle other routes or files as needed
+        res.writeHead(404, { 'Content-Type': 'text/plain' });
+        res.end('Not Found');
+    }
 });
 server.listen(port, hostname, () => {
  console.log(`Server running at http://${hostname}:${port}/`);
