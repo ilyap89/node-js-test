fde6d5b2227d68b188dd5ddbddcffd10e67fe583
ilyap89 <ilyaap89@gmail.com>
2024-01-03 15:57:00 +0200

    Create config.js

---
+++ config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    database: {
+        host: 'ilya-sql-server.cvm8q860ok2j.eu-west-1.rds.amazonaws.com',
+        user: 'admin',
+        password: 'Mezuki13121989!',
+        database: 'testDB',
+    },
+    s3: {
+        bucketName: 'ilya89testbucket',
+    },
+};
